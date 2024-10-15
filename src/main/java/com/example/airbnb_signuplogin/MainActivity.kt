9341641e35ead599efbfd94fc80d5ffe8c311b8a
Haroon Ur Rasheed <reach.haroonrasheed@gmail.com>
2024-10-16 00:51:04 +0500

    Add files via upload

---
+++ src/main/java/com/example/airbnb_signuplogin/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.airbnb_signuplogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.airbnb_signuplogin.ui.theme.AirBnBSignupLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AirBnBSignupLoginTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Haroon Ur Rasheed",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AirBnBSignupLoginTheme {
+        Greeting("Haroon Ur Rasheed")
+    }
+}
